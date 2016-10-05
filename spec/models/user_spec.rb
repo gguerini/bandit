7bb2418eb0821a986a9a954bfe97efe0f2cbb38e
Guillermo Guerini <guillermo@gguerini.com>
2016-10-05 19:22:38 -0400

    Add username to User.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,23 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_length_of(:username).is_at_least(1).is_at_most(30) }
+
+    it { should allow_value("foobar").for(:username) }
+    it { should allow_value("foobar99").for(:username) }
+    it { should_not allow_value("foobar!").for(:username) }
+    it { should_not allow_value("foo bar").for(:username) }
+    it { should_not allow_value("FooBar").for(:username) }
 
     it { should have_db_index(:email).unique(true) }
+    it { should have_db_index(:username).unique(true) }
 
     describe "uniqueness" do
       subject { FactoryGirl.build(:user) }
 
       it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+      it { should validate_uniqueness_of(:username).ignoring_case_sensitivity }
     end
   end
 end
